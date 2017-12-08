65f6339413e4b201a24c8a7ba9e844d0ca0aa60e
greegav <greegav@gmail.com>
2017-12-08 16:37:49 +0200

    jdbs_insert

--- Lectures_src/lecture-04-submitting-sql-queries/jdbc-query/src/JdbcDemo.java
+++ Lectures_src/lecture-04-submitting-sql-queries/jdbc-query/src/JdbcDemo.java
@@ -12,22 +12,31 @@ public class JdbcDemo {
 		Connection myConn = null;
 		Statement myStmt = null;
 		ResultSet myRs = null;
-		
-		String dbUrl = "jdbc:mysql://localhost:3306/demo";
-		String user = "student";		
+
+		String dbUrl = "jdbc:mysql://localhost:3306/demo?usessl=false";
+		String user = "student";
 		String pass = "student";
 
 		try {
 			// 1. Get a connection to database
 			myConn = DriverManager.getConnection(dbUrl, user, pass);
-			
+
 			// 2. Create a statement
 			myStmt = myConn.createStatement();
-			
-			// 3. Execute SQL query
-			myRs = myStmt.executeQuery("select * from employees");
-			
-			// 4. Process the result set
+
+			// 3. Insert a new employee
+			System.out.println("Inserting a new employee to database\n");
+
+			int rowsAffected = myStmt.executeUpdate(
+					"insert into employees " +
+							"(last_name, first_name, email, department, salary) " +
+							"values " +
+							"('SurnameTest', 'NameTest', 'test@email', 'TestPos', 999.99)");
+
+			// 4. Verify this by getting a list of employees
+			myRs = myStmt.executeQuery("select * from employees order by last_name");
+
+			// 5. Process the result set
 			while (myRs.next()) {
 				System.out.println(myRs.getString("last_name") + ", " + myRs.getString("first_name"));
 			}
@@ -39,11 +48,11 @@ public class JdbcDemo {
 			if (myRs != null) {
 				myRs.close();
 			}
-			
+
 			if (myStmt != null) {
 				myStmt.close();
 			}
-			
+
 			if (myConn != null) {
 				myConn.close();
 			}
